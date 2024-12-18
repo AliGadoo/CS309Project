a3fc511b88f4ede3e4138c15ddbba2dd4b19f7cb
Eslam Rasmy <eslamrasme@gmai.com>
2024-12-18 16:11:23 +0200

    add footer

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -9,6 +9,20 @@ const Home = () => {
     <div className="home-container">
       <h1>Home Page</h1>
       <ProductList />
+
+
+
+      <div class="last">
+        <div class="container">
+            <div class="last1">
+                <a href="#demo"> <i class="fa-brands fa-facebook-f"></i></a>
+                <a href="#demo"> <i class="fa-brands fa-twitter"></i></a>
+                <a href="#demo"><i class="fa-brands fa-google"></i></a>
+                <a href="#demo"> <i class="fa-brands fa-github"></i></a>
+            </div>
+            <p class="be">Copy Right 2018 © By<a href="#demo">Theme-fair</a> All Rights Reserved</p>
+        </div>
+    </div>
     </div>
   );
 };
