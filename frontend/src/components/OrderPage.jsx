2cd1fac73f2318a0fc6078ecb341271cad83dc1a
Eslam Rasmy <eslamrasme@gmai.com>
2024-12-21 02:13:48 +0200

    start of orderPage

--- frontend/src/components/OrderPage.jsx
+++ frontend/src/components/OrderPage.jsx
@@ -1,60 +1,34 @@
-import React, { useState } from 'react';
-import './OrderPage.css'; // استيراد ملف CSS
+import React, { useState } from "react";
+import "./OrderPage.css";
 
 function OrderPage() {
-  const [quantity, setQuantity] = useState("");
-  const [phoneNumber, setPhoneNumber] = useState("");
-  const [address, setAddress] = useState("");
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // هنا يمكن إرسال البيانات إلى الخادم أو معالجتها
-    console.log({ quantity, phoneNumber, address });
-  };
-
   return (
-    <div className="order-container">
-      <h1>Order Page</h1>
-      <p>Your order has been placed successfully!</p>
-
-      <form onSubmit={handleSubmit}>
-        <div className="form-group">
-          <label htmlFor="quantity">Quantity:</label>
-          <input
-            type="number"
-            id="quantity"
-            value={quantity}
-            onChange={(e) => setQuantity(e.target.value)}
-            required
-          />
+    <div className="container">
+      <h2>My orders</h2>
+      <div className="order">
+        <div className="order2">
+          <div className="order-item1">
+            {" "}
+            <h3>product 1</h3>
+          </div>
+          <div className="order-item2">
+            {" "}
+            <h4>T-shirt</h4>
+            <p>order:#1234</p>
+            <p>orderd on : Dec 10,2024</p>
+          </div>
         </div>
+        <div className="order1">
+          <button className="btn1">Panding</button>
+          <button className="btn2">Cancel</button>
+          <button className="btn3">Delvired</button>
+          <button className="btn4">Remove</button>
 
-        <div className="form-group">
-          <label htmlFor="phoneNumber">Phone Number:</label>
-          <input
-            type="tel"
-            id="phoneNumber"
-            value={phoneNumber}
-            onChange={(e) => setPhoneNumber(e.target.value)}
-            required
-          />
         </div>
-
-        <div className="form-group">
-          <label htmlFor="address">Address:</label>
-          <textarea
-            id="address"
-            value={address}
-            onChange={(e) => setAddress(e.target.value)}
-            required
-          ></textarea>
-        </div>
-
-        <button type="submit">Submit Order</button>
-      </form>
+      </div>
+      
     </div>
   );
 }
 
 export default OrderPage;
-
